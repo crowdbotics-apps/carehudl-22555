a58717b22b875c40b95ec75b55ba17ca12f79e33
Crowdbotics <team@crowdbotics.com>
2020-11-12 11:42:02 +0000

    Update models

--- backend/carehudl_22555/urls.py
+++ backend/carehudl_22555/urls.py
@@ -33,6 +33,11 @@ urlpatterns = [
     path("api/v1/", include("healthcare.api.v1.urls")),
     path("healthcare/", include("healthcare.urls")),
     path("home/", include("home.urls")),
+    path("api/v1/", include("chat.api.v1.urls")),
+    path("chat/", include("chat.urls")),
+    path("api/v1/", include("chat_user_profile.api.v1.urls")),
+    path("chat_user_profile/", include("chat_user_profile.urls")),
+    path("api/v1/", include("users.api.v1.urls")),
 ]
 
 admin.site.site_header = "CareHudl"
